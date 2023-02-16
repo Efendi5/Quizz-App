cf7b5d4ade3abc7cb557f12405eb05f7d04b41f4
Efendi Varol <efendiv@icloud.com>
2023-02-16 23:53:08 +0100

    change sm

--- js/index.js
+++ js/index.js
@@ -4,11 +4,7 @@ const revealButton = document.querySelector('[data-js="reveal-button"]');
 const cardAnswer = document.querySelector('[data-js="card-answer"]');
 
 revealButton.addEventListener("click", () => {
-  //card answer soll display flex machen
-  // entferne die klasse hidden von card answer
   cardAnswer.classList.toggle(["visible"]);
-  // wenn klasse von cardAnswer hidden ist,
-  //soll im button der text hide answer stehen
 
   if (cardAnswer.classList.contains("visible")) {
     revealButton.textContent = "Hide answer";
@@ -16,3 +12,8 @@ revealButton.addEventListener("click", () => {
     revealButton.textContent = "Show answer";
   }
 });
+
+// Mit dem addEventListener wird dem Button durch das Klick event die Fähigkeit gegeben
+// der card answer die hidden Klasse zu entfernen und die display flex Klasse zu geben.
+// Anschließend wird mit der if/else funktion der card answer Klasse: Bei visible (true) -> Hide Answer
+// und bei else in dem Fall (false) die Antwort zu zeigen.
--- js/selectedProfil.js
+++ js/selectedProfil.js
@@ -6,3 +6,5 @@ profilButton.addEventListener("click", () => {
   profilButton.classList.add("active-choice");
   console.log("Selected Profil");
 });
+
+// Durch den profilButton mit der Klasse active-choise wird in der Nav Leiste angeziegt welche Seite aktuell angezeigt wird
